6ea350d52e970bc3d2505aa595ad0fa8bd7dd63e
alsduq1117 <alsduq1117@naver.com>
2024-11-05 02:11:18 +0900

    problem 02

--- src/main/java/problem/easy/Problem2.java
+++ src/main/java/problem/easy/Problem2.java
@@ -1,6 +1,7 @@
 package problem.easy;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem2 {
 
@@ -11,7 +12,8 @@ public class Problem2 {
      * @return 각 요소를 제곱한 새 리스트
      */
     public static List<Integer> squareNumbers(List<Integer> numbers) {
-        // 여기에 코드 작성
-        return null;
+        return numbers.stream()
+                .map(number -> number * number)
+                .collect(Collectors.toList());
     }
 }
