1b418ee87bc876e1d1bd9a046c26196a36ea47c0
alsduq1117 <alsduq1117@naver.com>
2024-11-06 13:41:10 +0900

    problem13 - Queue에 있는 모든 정수에 2를 곱하여 새 Queue를 생성

--- src/main/java/problem/easy/Problem13.java
+++ src/main/java/problem/easy/Problem13.java
@@ -2,6 +2,7 @@ package problem.easy;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class Problem13 {
 
@@ -12,7 +13,10 @@ public class Problem13 {
      * @return 각 요소에 2를 곱한 새 Queue
      */
     public static Queue<Integer> doubleEach(Queue<Integer> numbers) {
-        // 여기에 코드 작성
-        return new LinkedList<>();
+        return numbers.stream()
+                .map(number -> number * 2)
+                .collect(Collectors.toCollection(LinkedList::new));
     }
+
+    //인터페이스 Queue 의 구현체인 LinkedList 로 변환해서 반환해야 한다.
 }
