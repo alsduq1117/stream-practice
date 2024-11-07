3a11b4803fa51420e8735b71749622c9430eae18
alsduq1117 <alsduq1117@naver.com>
2024-11-07 16:26:23 +0900

    problem20 - 스트림을 사용하여 정수 배열에서 LinkedHashMap을 생성

--- src/main/java/problem/easy/Problem20.java
+++ src/main/java/problem/easy/Problem20.java
@@ -1,9 +1,12 @@
 package problem.easy;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem20 {
-
+    // 다시 풀어보기
     /**
      * 스트림을 사용하여 정수 배열에서 LinkedHashMap을 생성합니다.
      * 배열의 각 요소를 key로 하고, 해당 요소를 제곱한 값을 value로 하는 LinkedHashMap을 반환합니다.
@@ -12,7 +15,13 @@ public class Problem20 {
      * @return 생성된 LinkedHashMap
      */
     public static Map<Integer, Integer> createLinkedHashMapFromStream(int[] numbers) {
-        // 여기에 코드 작성
-        return null;
+        return Arrays.stream(numbers)
+                .boxed()
+                .collect(Collectors.toMap(
+                        number -> number,
+                        number -> number*number,
+                        (existing, replacement) -> existing,
+                        LinkedHashMap::new
+                ));
     }
 }
