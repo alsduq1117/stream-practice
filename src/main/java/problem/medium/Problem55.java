031846b9b0ddb292b5deb8813927d303b21e2fb5
alsduq1117 <alsduq1117@naver.com>
2024-11-18 08:25:15 +0900

    problem55 : 'Bread' 제품을 가장 많이 주문한 고객의 이름

--- src/main/java/problem/medium/Problem55.java
+++ src/main/java/problem/medium/Problem55.java
@@ -1,7 +1,9 @@
 package problem.medium;
 
+import java.util.Comparator;
 import java.util.List;
 import problem.medium.resources.Customer2;
+import problem.medium.resources.Order;
 
 public class Problem55 {
 
@@ -13,7 +15,14 @@ public class Problem55 {
      * @return 'Bread' 제품을 가장 많이 주문한 고객의 이름
      */
     public static String getCustomerWhoOrderedMostBread(List<Customer2> customers) {
-        // 여기에 코드 작성
-        return "";
+        return customers.stream()
+                .max(Comparator.comparingInt(customer ->
+                        customer.getOrders().stream()
+                                .filter(order -> order.getProduct().equalsIgnoreCase("Bread"))
+                                .mapToInt(Order::getQuantity)
+                                .sum()
+                ))
+                .map(Customer2::getName) // 가장 많이 주문한 고객의 이름 가져오기
+                .orElse(""); // 만약 고객이 없다면 빈 문자열 반환
     }
 }
