7ed39dd6b825057eab679f786e376ae2e9981ed5
alsduq1117 <alsduq1117@naver.com>
2024-11-20 02:49:25 +0900

    problem57 : 'Coffee' 제품을 포함하는 주문 리스트

--- src/main/java/problem/medium/Problem57.java
+++ src/main/java/problem/medium/Problem57.java
@@ -1,7 +1,9 @@
 package problem.medium;
 
-import java.util.List;
 import problem.medium.resources.Order;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem57 {
 
@@ -12,7 +14,8 @@ public class Problem57 {
      * @return 'Coffee' 제품을 포함하는 주문 리스트
      */
     public static List<Order> findOrdersWithCoffee(List<Order> orders) {
-        // 여기에 코드 작성
-        return null;
+        return orders.stream()
+                .filter(order -> order.getProduct().equalsIgnoreCase("Coffee"))
+                .collect(Collectors.toList());
     }
 }
